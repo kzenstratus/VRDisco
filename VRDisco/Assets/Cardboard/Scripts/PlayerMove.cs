a878af2fa8de15c1dfa15587a9c179b0611becec
Kevin <kzenstratus@uchicago.edu>
2016-02-25 19:42:00 -0600

    wonky head movement

---
+++ VRDisco/Assets/Cardboard/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+	public GameObject SpawnPoint;
+
+	void Start()
+	{
+		transform.position = SpawnPoint.transform.localPosition;
+
+	}
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+		
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
---
+++ VRDisco/Assets/Cardboard/Scripts/TrackPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TrackPlayer : NetworkBehaviour {
+	private CardboardHead head;
+
+	private Vector3 offset;
+	public GameObject me;
+
+	void Start()  {
+		head =Camera.main.GetComponent<StereoController>().Head;
+		me =GameObject.FindWithTag("Player");
+	}
+
+	void LateUpdate() {
+
+		// head.transform.position = thelook positon of the head on the plane
+		// head.Gaze.direction = positon of where the head is looking
+		if (me != null) {
+			offset = head.Gaze.direction + head.transform.position;
+//			playerObject.transform.Translate (direction);
+			me.transform.position = me.transform.position + offset;
+
+
+		}
+	}
+}
