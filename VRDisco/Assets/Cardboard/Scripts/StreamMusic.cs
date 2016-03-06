91b3952db8fc8327f137b1f31b663cf1a356be17
Max Chen <mchen24@uchicago.edu>
2016-03-06 02:08:57 -0600

    array of music set up, music should play on multiple clients. Need to add name tags, lights, and rigidbody for walls

--- VRDisco/Assets/Cardboard/Scripts/StreamMusic.cs
+++ VRDisco/Assets/Cardboard/Scripts/StreamMusic.cs
@@ -2,142 +2,67 @@
 using System.Collections;
 using System.IO;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
 
 public class StreamMusic : NetworkBehaviour {
 
-	//    public string url1 = "http://149.13.0.80:80/radio1rock.ogg";
-	//    public string url2 = "http://ai-radio.org:8000/128.ogg";
-	//    public string url3 = "http://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg";
-	//    public string url4 = "http://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg";
-	//    public string url5 = "http://www.stephaniequinn.com/Music/Vivaldi%20-%20Spring%20from%20Four%20Seasons.mp3";
-	//    public string url5;
-	//    private string tempUrl = "http://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg";
-	//    private string tempUrl2 = "http://www.stephaniequinn.com/Music/Vivaldi%20-%20Spring%20from%20Four%20Seasons.mp3";
-	//    public string url6 = "http://www.stephaniequinn.com/Music/Brahms%20Symphony%20-%20from%20Fourth%20Movement.mp3";
-	//
 
 	[SyncVar]
 	public string songName;
 
-	[SyncVar]
-	public AudioSource source1;
+//	public string songName;
+	int currentIndex;
+
+//	[SyncVar]
+//	public AudioSource source1;
 
 
-	public string sorry = "I'm so sorry";
-	public string someone = "Someone like you";
-
-
-	//    = "http://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg";
-	//    public string url2 = "/Music/leave_a_trace.ogg";
+	List<string> songs = new List<string>();
 
 	void Start(){
-		//        if (Application.platform == RuntimePlatform.Android) {
-		//            print ("Android");
-		////            url5 = "http://www.stephaniequinn.com/Music/Pachelbel%20-%20Canon%20in%20D%20Major.mp3";
-		//            url6 = "http://www.stephaniequinn.com/Music/Brahms%20Symphony%20-%20from%20Fourth%20Movement.mp3";
-		//
-		//        } else if (Application.platform == RuntimePlatform.OSXEditor) {
-		//            print ("On the mac");
-		//            url5 = "http://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg";
-		//            url6 = "http://www.stephaniequinn.com/Music/Brahms%20Symphony%20-%20from%20Fourth%20Movement.mp3";
-		////            url6 = "http://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg";
-		//
-		//        } else if (Application.platform == RuntimePlatform.OSXPlayer) {
-		//            url5 = "http://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg";
-		//            url6 = "http://www.stephaniequinn.com/Music/Brahms%20Symphony%20-%20from%20Fourth%20Movement.mp3";
-		//
-		////            url6 = "http://upload.wikimedia.org/wikipedia/en/9/9f/Sample_of_%22Another_Day_in_Paradise%22.ogg";
-		//
-		//        }
-
-		songName = someone;
+		songs.Add("Faded");
+		songs.Add("Happiest man on earth");
+		songs.Add("Leave a trace");
+		songs.Add("Open Season");
+		songs.Add("Surreal");
+		songs.Add("The Hum");
 	}
 
-	public void updateMusic(){
-		if (!isLocalPlayer)
-			return;
-		//        if (Application.platform == RuntimePlatform.Android) {
-		//            url6 = newSongMobile;
-		//        } else if (Application.platform == RuntimePlatform.OSXEditor) {
-		//            url6 = newSongMobile;
-		//            print (url6);
-		//
-		//        } else if (Application.platform == RuntimePlatform.OSXPlayer) {
-		//            url6 = newSongMobile;
-		//
-		//        }
-
-	}
 
 	[ClientRpc]
-	public void RpcPlaySong(){
-		songName = someone;
-		source1 = GetComponent<AudioSource>();
+	public void RpcPlaySong(string current){
+
+		songName = current;
+		AudioSource source1 = GetComponent<AudioSource>();
 		source1.loop = true;
 		source1.clip = Resources.Load(songName) as AudioClip;
 		source1.Play ();
 	}
 
-	[ServerCallback]
+	[ClientRpc]
+	public void RpcStopSong(){
+		AudioSource s2;
+		s2 = GetComponent<AudioSource>();
+		s2.Stop ();
+	}
+
+	[Server]
 	void Update()
 	{
 		if (Input.GetKeyDown (KeyCode.N)) 
 		{
-			RpcPlaySong();
+			currentIndex = (currentIndex + 1) % songs.Count;
+			string current = songs[currentIndex];
+			RpcPlaySong(current);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			RpcStopSong();
 		}
 
-		//        if (Input.GetKeyDown(KeyCode.N)) this.Tune(this.nnurl1);
-		//        if (Input.GetKeyDown(KeyCode.M)) this.Tune(this.url3);
-		//        if (Input.GetKeyDown(KeyCode.N)) this.Tune(this.url4);
-		//        if (Input.GetKeyDown(KeyCode.B)) this.Tune(this.url5);
-		//        this.Tune (this.url5);
-
-		//        if (Input.GetKeyDown(KeyCode.B)) this.Tune(this.url2);
 	}
-
-	public void Tune(string url)
-	{
-		//print ("My current url is " + url);
-		this.StopAllCoroutines();
-		//this.StartCoroutine(this.__Tune(url5));
-		//        this.StartCoroutine(this.__Tune("When I was your man"));
-
-		source1 = GetComponent<AudioSource>();
-		source1.loop = true;
-		source1.clip = Resources.Load(songName) as AudioClip;
-		source1.Play ();
-	}
-	//    IEnumerator __Tune(string url)
-	//    {
-	////        var www = new WWW(url);
-	////
-	////        var source = GetComponent<AudioSource>();
-	////
-	//////        source.clip = www.GetAudioClip (false, false, AudioType.OGGVORBIS);
-	////        source.clip = www.GetAudioClip (false, false);
-	////
-	//////
-	////        while (!(source.clip.loadState == AudioDataLoadState.Loaded)) {
-	////            yield return null;
-	////        }
-	////
-	////        this.GetComponent<AudioSource>().clip = source.clip;
-	////        this.GetComponent<AudioSource>().Play ();
-	//    }
-
+		
 }
 
 
-//public class StreamMusic : MonoBehaviour {
-//
-//    // Use this for initialization
-//    void Start () {
-//    
-//    }
-//    
-//    // Update is called once per frame
-//    void Update () {
-//    
-//    }
-//}
