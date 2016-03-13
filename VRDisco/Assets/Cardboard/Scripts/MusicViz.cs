aa4fcc5579d8a618de6915d9fd3d1ddecd005a9e
Kevin <kzenstratus@uchicago.edu>
2016-03-12 22:05:08 -0600

    WORKING mobile client music visualization

--- VRDisco/Assets/Cardboard/Scripts/MusicViz.cs
+++ VRDisco/Assets/Cardboard/Scripts/MusicViz.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 public class MusicViz : MonoBehaviour {
 
@@ -13,10 +14,12 @@ public class MusicViz : MonoBehaviour {
 	void Start () {
 		for(int i = 1; i< numberObjects; i ++){
 			float angle = i * Mathf.PI * 2 /numberObjects;
-			Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)+0.3f,1) * radius;
+			Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)+0.3f,0.8f) * radius;
 			Instantiate(bar,pos, Quaternion.identity);
 		}
 		cubes = GameObject.FindGameObjectsWithTag ("musicviz");
+
+
 	}
 	
 	// Update is called once per frame
@@ -25,8 +28,10 @@ public class MusicViz : MonoBehaviour {
 		int channel = 0;
 		float amp = 0f;
 		AudioListener.GetOutputData(spectrum,channel);
+
 		for(int i = 0; i < numberObjects; i++) {
 			Vector3 previousScale = cubes[i].transform.localScale;
+		
 			amp = Mathf.Pow (spectrum [i]*10f, 2f)/10f;
 
 			previousScale.x = Mathf.Lerp(previousScale.x, amp,Time.deltaTime * 30);
